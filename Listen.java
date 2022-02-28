fc2f8a3cc354a6ded45430390bb0198509d02ee3
Jay Sharma <sharmakjay@gmail.com>
2022-02-28 17:44:38 -0500

    Update Listen.java

--- Listen.java
+++ Listen.java
@@ -2,4 +2,23 @@ import jm.music.data.Note;
 import jm.util.Play;
 
 import jm.JMC;
+
 import java.util.TimerTask;
+
+public class Music extends TimerTask implements JMC {
+   private static final int[] SONG = {78, 78, 78, 72, 76, 79, 67, 72, 67, 64, 69, 71, 70, 69, 67, 76, 79, 81, 77, 79, 76};
+
+    private static int note;
+
+    @Override
+    public void run() {
+        try {
+                for (int i = 0; i < SONG.length; i++) {
+                    note = SONG[i];
+                    Play.midi(new Note(note, 0.01));
+                }
+        } catch(Exception ex) {
+            System.out.println("Error!");;
+        }
+    }
+}
