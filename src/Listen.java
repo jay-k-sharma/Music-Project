c1cce6f6df47d49ff7939ff481b87e2dab34d6d6
Jay Sharma <sharmakjay@gmail.com>
2022-04-16 17:58:36 -0400

    Update Listen.java

--- src/Listen.java
+++ src/Listen.java
@@ -8,5 +8,6 @@ import java.util.TimerTask;
 public class Music extends TimerTask implements JMC {
    private static final int[] SONG = {78, 78, 78, 72, 76, 79, 67, 72, 67, 64, 69, 71, 70, 69, 67, 76, 79, 81, 77, 79, 76};
 
+    private static int note;
 
 }
